20c67397a76c40b05b7b044b77e02ec39b733456
Elizabeth Case <5726652+Elizabethcase@users.noreply.github.com>
2024-06-20 15:41:56 +0200

    transfer from rapid to ecmwf

--- temp/transfer_RACMO2p32.sh
+++ temp/transfer_RACMO2p32.sh
@@ -1,17 +1,17 @@
 #/bin/sh
 
-rsync -avz --partial --append-verify /Volumes/imau01/rapid/RACMO2.3p2/FGRN055_1940/3-hourly/snowmelt.KNMI-2021*.nc hpc-login:/ec/res4/scratch/nld4814/RACMO23p2_GR/raw/FGRN055/decadal/
-rsync -avz --partial --append-verify /Volumes/imau01/rapid/RACMO2.3p2/FGRN055_1940/3-hourly/snowfall.KNMI-2021*.nc hpc-login:/ec/res4/scratch/nld4814/RACMO23p2_GR/raw/FGRN055/decadal/
-rsync -avz --partial --append-verify /Volumes/imau01/rapid/RACMO2.3p2/FGRN055_1940/3-hourly/tskin.KNMI-2021*.nc hpc-login:/ec/res4/scratch/nld4814/RACMO23p2_GR/raw/FGRN055/decadal/
-rsync -avz --partial --append-verify /Volumes/imau01/rapid/RACMO2.3p2/FGRN055_1940/3-hourly/evap.KNMI-2021*.nc hpc-login:/ec/res4/scratch/nld4814/RACMO23p2_GR/raw/FGRN055/decadal/
-rsync -avz --partial --append-verify /Volumes/imau01/rapid/RACMO2.3p2/FGRN055_1940/3-hourly/precip.KNMI-2021*.nc hpc-login:/ec/res4/scratch/nld4814/RACMO23p2_GR/raw/FGRN055/decadal/
-rsync -avz --partial --append-verify /Volumes/imau01/rapid/RACMO2.3p2/FGRN055_1940/3-hourly/ff10m.KNMI-2021*.nc hpc-login:/ec/res4/scratch/nld4814/RACMO23p2_GR/raw/FGRN055/decadal/
-rsync -avz --partial --append-verify /Volumes/imau01/rapid/RACMO2.3p2/FGRN055_1940/3-hourly/sndiv.KNMI-2021*.nc hpc-login:/ec/res4/scratch/nld4814/RACMO23p2_GR/raw/FGRN055/decadal/
 
-rsync -avz --partial --append-verify /Volumes/imau01/rapid/RACMO2.3p2/FGRN055_1940/3-hourly/NC_FDM-3h/snowmelt.KNMI-2023*.nc hpc-login:/ec/res4/scratch/nld4814/RACMO23p2_GR/raw/FGRN055/yearly/
-rsync -avz --partial --append-verify /Volumes/imau01/rapid/RACMO2.3p2/FGRN055_1940/3-hourly/NC_FDM-3h/snowfall.KNMI-2023*.nc --append hpc-login:/ec/res4/scratch/nld4814/RACMO23p2_GR/raw/FGRN055/yearly/
-rsync -avz --partial --append-verify N/Volumes/imau01/rapid/RACMO2.3p2/FGRN055_1940/3-hourly/NC_FDM-3h/tskin.KNMI-2023*.nc hpc-login:/ec/res4/scratch/nld4814/RACMO23p2_GR/raw/FGRN055/yearly/
-rsync -avz --partial --append-verify /Volumes/imau01/rapid/RACMO2.3p2/FGRN055_1940/3-hourly/NC_FDM-3h/evap.KNMI-2023*.nc hpc-login:/ec/res4/scratch/nld4814/RACMO23p2_GR/raw/FGRN055/yearly/
-rsync -avz --partial --append-verify /Volumes/imau01/rapid/RACMO2.3p2/FGRN055_1940/3-hourly/NC_FDM-3h/precip.KNMI-2023*.nc hpc-login:/ec/res4/scratch/nld4814/RACMO23p2_GR/raw/FGRN055/yearly/
-rsync -avz --partial --append-verify /Volumes/imau01/rapid/RACMO2.3p2/FGRN055_1940/3-hourly/NC_FDM-3h/ff10m.KNMI-2023*.nc hpc-login:/ec/res4/scratch/nld4814/RACMO23p2_GR/raw/FGRN055/yearly/
-rsync -avz --partial --append-verify /Volumes/imau01/rapid/RACMO2.3p2/FGRN055_1940/3-hourly/NC_FDM-3h/sndiv.KNMI-2023*.nc hpc-login:/ec/res4/scratch/nld4814/RACMO23p2_GR/raw/FGRN055/yearly/
+varlist=(evap ff10m precip sndiv snowfall snowmelt tskin)
+#decadelist=(1939 1941 1951 1961 1971 1981 1991 2001 2011 2021)
+decadelist=(1961 1971 1981 1991 2001 2011)
+
+for d in ${decadelist[@]}; do
+	for var in ${varlist[@]}; do
+		#echo ${var}.KNMI-${d}
+		rsync -avz --partial --append-verify /Volumes/imau01/rapid/RACMO2.3p2/FGRN055_1940/3-hourly/${var}.KNMI-${d}*.nc hpc-login:/ec/res4/scratch/nld4814/RACMO23p2_GR/raw/historical/decadal/
+	done
+done
+# for just 2023
+# for var in ${varlist}; do
+	# rsync -avz --partial --append-verify /Volumes/imau01/rapid/RACMO2.3p2/FGRN055_1940/3-hourly/NC_FDM-3h/${var}.KNMI-2023*.nc hpc-login:/ec/res4/scratch/nld4814/RACMO23p2_GR/raw/FGRN055/yearly/
+# done
